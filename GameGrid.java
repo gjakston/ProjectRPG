3a56726ad6b546ca375810dc917941466a9552b2
Gediminas Jakstonis <jakstonisg@s.dcsdk12.org>
2013-04-25 14:48:16 -0600

    Version Pre-Alpha 6b

--- GameGrid.java
+++ GameGrid.java
@@ -64,20 +64,21 @@ public class GameGrid {
                     }
                     break;
                 }
-            //RandomEvent(int x, int y)
             if (randomEventarizer.newRandomEvent(mainChar.LocationX(), mainChar.LocationY())== true) {
-                
+                if (identifyEvent() == 1) {
+                    //Monster Combat
+                    Monster monster = new Monster(
+                } else if (identifyEvent() == 2) {
+                    //NPC Combat
+                    
+                } else if (identifyEvent() == 3) {
+                    //Peaceful Animal Combat
+                    
+                } else if (identifyEvent() == 4) {
+                    //Town Interactions
+                    
+                }
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             
             
             
@@ -85,22 +86,67 @@ public class GameGrid {
             
         }  
     }
-    public static boolean isTown() {
+    public static Monster recreateMonster() {
         try {
             File file = new File("TemporaryData.gand");
             RandomAccessFile tempData = new RandomaAccessFile(file, "r");
             String readTempFile = tempData.readLine();
             String[] splitInformation = readTempFile.split(" ");
-            if (splitInformation.length == 5) {
-                return true;
+        } catch (Exception e) {
+            System.out.println("Exception: ");
+        }
+    }
+    public static NonPlayerCharacter recreateNPC() {
+        try {
+            File file = new File("TemporaryData.gand");
+            RandomAccessFile tempData = new RandomaAccessFile(file, "r");
+            String readTempFile = tempData.readLine();
+            String[] splitInformation = readTempFile.split(" ");
+        } catch (Exception e) {
+            System.out.println("Exception: ");
+        }
+    }
+    public static PeacefulAnimal recreatePeacefulAnimal() {
+        try {
+            File file = new File("TemporaryData.gand");
+            RandomAccessFile tempData = new RandomaAccessFile(file, "r");
+            String readTempFile = tempData.readLine();
+            String[] splitInformation = readTempFile.split(" ");
+        } catch (Exception e) {
+            System.out.println("Exception: ");
+        }
+    }
+    public static Town recreateTown() {
+        try {
+            File file = new File("TemporaryData.gand");
+            RandomAccessFile tempData = new RandomaAccessFile(file, "r");
+            String readTempFile = tempData.readLine();
+            String[] splitInformation = readTempFile.split(" ");
+        } catch (Exception e) {
+            System.out.println("Exception: ");
+        }
+    }
+    /**Identifies the type of random event created */
+    public static byte identifyEvent() {
+        try {
+            File file = new File("TemporaryData.gand");
+            RandomAccessFile tempData = new RandomaAccessFile(file, "r");
+            String readTempFile = tempData.readLine();
+            String[] splitInformation = readTempFile.split(" ");
+            if (splitInformation.length == 4) { // Peaceful Animal
+                return 3;
+            } else if (splitInformation.length == 5) { // Town
+                return 4;
+            } else if (splitInformation.length == 7) { // Monster
+                return 0;
+            } else if (splitInformation.length == 8) { // NPC 
+                return 1;
             } else {
-                return false;
+                return -1;
             }
-            
         } catch (Exception e) {
             System.out.println("IOException: ");
         }
-        
     }
     /**
      * Controls and manipulates the cognitive values of the game engine, determining indirectly the likelyhood of a new instantiation of a conceptualizer object that controls the probability of another call to the conceptualizer method in the same class.
@@ -109,7 +155,7 @@ public class GameGrid {
     public void Conceptualizer(String toStringResultValue, int seed, int probability, double[][][][] theoreticalSpawns, Object caricature) {
         int g = (int)((Math.pow(2, 32) - 1) / 2);
         long d = (long)Math.sqrt(Math.PI);
-        int Au = 79;
+        int Au = 79; // NingCsweey 
         int Ag = 47;
         int Hg = Au*Ag;
         while(g > 0) {
